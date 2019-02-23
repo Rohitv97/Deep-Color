c9e7b1969fa0fcf77a3d9b9f6e841444953cb060
Rohitv97 <vrohit97.rv@gmail.com>
2019-02-23 13:01:39 +0530

    Added trained model file for CNN

---
+++ CNN/v1_c.py
@@ -0,0 +1,55 @@
+from keras.layers import Conv2D, Conv2DTranspose, UpSampling2D
+from keras.layers import Activation, Dense, Dropout, Flatten, InputLayer
+from keras.layers.normalization import BatchNormalization
+from keras.callbacks import TensorBoard
+from keras.models import Sequential
+from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
+from skimage.color import rgb2lab, lab2rgb, rgb2gray
+from skimage.io import imsave
+import numpy as np
+import os
+import random
+import tensorflow as tf
+
+# Initialising and loading Model
+
+model = Sequential()
+model.add(InputLayer(input_shape=(256, 256, 1)))
+model.add(Conv2D(64, (3, 3), activation='relu', padding='same'))
+model.add(Conv2D(64, (3, 3), activation='relu', padding='same', strides=2))
+model.add(Conv2D(128, (3, 3), activation='relu', padding='same'))
+model.add(Conv2D(128, (3, 3), activation='relu', padding='same', strides=2))
+model.add(Conv2D(256, (3, 3), activation='relu', padding='same'))
+model.add(Conv2D(256, (3, 3), activation='relu', padding='same', strides=2))
+model.add(Conv2D(512, (3, 3), activation='relu', padding='same'))
+model.add(Conv2D(256, (3, 3), activation='relu', padding='same'))
+model.add(Conv2D(128, (3, 3), activation='relu', padding='same'))
+model.add(UpSampling2D((2, 2)))
+model.add(Conv2D(64, (3, 3), activation='relu', padding='same'))
+model.add(UpSampling2D((2, 2)))
+model.add(Conv2D(32, (3, 3), activation='relu', padding='same'))
+model.add(Conv2D(2, (3, 3), activation='tanh', padding='same'))
+model.add(UpSampling2D((2, 2)))
+model.compile(optimizer='rmsprop', loss='mse')
+
+model.load_weights("model.h5")
+
+# Change to '/data/images/Test/' to use all the 500 images
+color_me = []
+for filename in os.listdir('test/'):
+	color_me.append(img_to_array(load_img('test/'+filename)))
+color_me = np.array(color_me, dtype=float)
+color_me = rgb2lab(1.0/255*color_me)[:,:,:,0]
+color_me = color_me.reshape(color_me.shape+(1,))
+
+# Test model
+output = model.predict(color_me)
+output = output * 128
+
+# Output colorizations
+
+for i in range(len(output)):
+	cur = np.zeros((256, 256, 3))
+	cur[:,:,0] = color_me[i][:,:,0]
+	cur[:,:,1:] = output[i]
+	imsave("result/img_"+str(i)+".png", lab2rgb(cur))
